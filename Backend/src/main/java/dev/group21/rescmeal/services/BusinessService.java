2bc531f1df980508aa575ea1d802fb901666d2a2
Alan Cristian Gaston Vera <vera.alan2207@gmail.com>
2024-09-22 18:00:51 -0300

    Documented Service methods. Removed unused properties in application.properties

--- Backend/src/main/java/dev/group21/rescmeal/services/BusinessService.java
+++ Backend/src/main/java/dev/group21/rescmeal/services/BusinessService.java
@@ -4,28 +4,52 @@ import dev.group21.rescmeal.model.Business;
 import dev.group21.rescmeal.repository.BusinessPhotoRepository;
 import dev.group21.rescmeal.repository.BusinessRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+@Transactional
 public class BusinessService {
     private final BusinessRepository businessRepository;
     private final BusinessPhotoRepository businessPhotoRepository;
 
+    /**
+     * Init methods for Business Service.
+     * @param businessRepository Business Repository.
+     * @param businessPhotoRepository BusinessPhoto Repository.
+     */
     public BusinessService(BusinessRepository businessRepository, BusinessPhotoRepository businessPhotoRepository) {
         this.businessRepository = businessRepository;
         this.businessPhotoRepository = businessPhotoRepository;
     }
 
-    public Business creteBusiness(Business business) {
+    /**
+     * Create a Business entity and BusinessPhoto relation.
+     * @param business complete Business entity.
+     * @return Business entity
+     */
+    public Business createBusiness(Business business) {
+        businessPhotoRepository.save(business.getBusinessPhoto());
         return businessRepository.save(business);
     }
 
+    /**
+     * Update method, needs a complete Business entity. No null properties.
+     * @param updateBusiness Business entity with updated values.
+     * @return Business entity
+     */
     public Business updateBusiness(Business updateBusiness) {
         businessPhotoRepository.saveAndFlush(updateBusiness.getBusinessPhoto());
         return businessRepository.saveAndFlush(updateBusiness);
     }
 
+    /**
+     * Update method, works even while sending a partially different Business entity.
+     * @param existingBusiness old Business entity without changed values.
+     * @param updateBusiness updated Business entity, may have null properties.
+     * @return Business entity
+     */
     public Business dynamicUpdateBusiness(Business existingBusiness, Business updateBusiness) {
         // Update fields that were sent with some value.
         if(updateBusiness.getBusiness_name() == null) updateBusiness.setBusiness_name(existingBusiness.getBusiness_name());
@@ -41,15 +65,28 @@ public class BusinessService {
         return businessRepository.saveAndFlush(updateBusiness);
     }
 
+    /**
+     * Delete a Business entity by id and its BusinessPhoto relation.
+     * @param id Integer id of Business entity.
+     */
     public void deleteBusiness(Integer id) {
         businessPhotoRepository.deleteById(id);
         businessRepository.deleteById(id);
     }
 
+    /**
+     * Get method for Business entity, finds by entity id.
+     * @param id Integer id or Business entity.
+     * @return Business entity.
+     */
     public Business getBusiness(Integer id) {
         return businessRepository.findById(id).orElse(null);
     }
 
+    /**
+     * List all Business entity present in database.
+     * @return List of Business entities.
+     */
     public List<Business> getAllBusiness() {
         return businessRepository.findAll();
     }
