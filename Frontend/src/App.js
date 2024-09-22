2d407c5c98e81caf4df985c3748d00fbe4d6c54b
acostaDemianAaron <acostademianaaron14@gmail.com>
2024-09-21 21:41:39 -0300

    Probando respuestas con el Backend

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,9 +1,23 @@
-function App() {
-  return (
-    <div className="App">
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ClientList from './components/ClientList';
+import CreateClient from './components/CreateClient';
+import UpdateClient from './components/UpdateClient';
+import DeleteClient from './components/DeleteClient';
 
-    </div>
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<ClientList />} />
+          <Route path="/create" element={<CreateClient />} />
+          <Route path="/update/:id" element={<UpdateClient />} />
+          <Route path="/delete/:id" element={<DeleteClient />} />
+        </Routes>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ Frontend/src/components/DeleteClient.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import axios from '../api/axiosConfig';
+
+const DeleteClient = () => {
+    const { id } = useParams();
+    console.log('Received ID:', id);
+
+    const handleDelete = () => {
+        if (id === undefined) {
+            console.error('Error: id is undefined');
+            return;
+        }
+        axios.delete(`/clients/${id}`)
+            .then(response => console.log('Client deleted:', response.data))
+            .catch(error => console.error('Error deleting client:', error));
+    };
+
+    return (
+        <button onClick={handleDelete}>Delete Client</button>
+    );
+};
+
+export default DeleteClient;
+
