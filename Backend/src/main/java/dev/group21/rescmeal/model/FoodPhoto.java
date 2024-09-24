4f7b4ba12a4afdd96239c66cb37a06d2a76acc4b
acostaDemianAaron <acostademianaaron14@gmail.com>
2024-09-24 01:52:28 -0300

    Se cargo Food Completo del JPA con Photo Revisar

--- Backend/src/main/java/dev/group21/rescmeal/model/FoodPhoto.java
+++ Backend/src/main/java/dev/group21/rescmeal/model/FoodPhoto.java
@@ -1,27 +1,23 @@
-/*
 package dev.group21.rescmeal.model;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Table(name = "food_image")
-public class FoodImage {
+@Table(name = "food_photo")
+public class FoodPhoto {
     @Id
     private Long food_id;
 
     @Lob
-    private byte[] image;
+    private byte[] photo;
 
     @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "idFood") // TODO Nombre de la columna en Food para el id.
+    @JoinColumn(name = "food_id")
     @JsonBackReference
     private  Food food;
 }
-*/
