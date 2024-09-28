6c3b8ba7fca1133b8fe52ab6707d6b8dcce9dad8
acostaDemianAaron <acostademianaaron14@gmail.com>
2024-09-27 21:34:19 -0300

    Refactor de Client y Food.

--- Backend/src/main/java/dev/group21/rescmeal/controller/ClientController.java
+++ Backend/src/main/java/dev/group21/rescmeal/controller/ClientController.java
@@ -1,64 +1,111 @@
 package dev.group21.rescmeal.controller;
 
 import dev.group21.rescmeal.model.Client;
-import dev.group21.rescmeal.repository.ClientRepository;
+import dev.group21.rescmeal.services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
-@RequestMapping("/clients")
+@RequestMapping("/api/clients")
 public class ClientController {
+    private final ClientService clientService;
 
     @Autowired
-    private ClientRepository clientRepository;
-
-    @GetMapping
-    public List<Client> getAllClients() {
-        return clientRepository.findAll();
-    }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<Client> getClientById(@PathVariable Integer id) {
-        Optional<Client> client = clientRepository.findById(id);
-        return client.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    public ClientController(ClientService clientService) {
+        this.clientService = clientService;
     }
 
     @PostMapping
-    public Client createClient(@RequestBody Client client) {
-        return clientRepository.save(client);
+    public ResponseEntity<Client> createClient(@RequestBody Client client) {
+        try {
+            Client createdClient = clientService.createClient(client);
+            return ResponseEntity.ok(createdClient);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).headers(errorHeader(e)).build();
+        }
     }
 
-    @PutMapping("/{id}")
-    public ResponseEntity<Client> updateClient(@PathVariable Integer id, @RequestBody Client clientDetails) {
-        Optional<Client> client = clientRepository.findById(id);
-        if (client.isPresent()) {
-            Client updatedClient = client.get();
-            updatedClient.setName(clientDetails.getName());
-            updatedClient.setLastName(clientDetails.getLastName());
-            updatedClient.setEmail(clientDetails.getEmail());
-            updatedClient.setPhone(clientDetails.getPhone());
-            updatedClient.setPassword(clientDetails.getPassword());
-            updatedClient.setBalance(clientDetails.getBalance());
-            updatedClient.setAddress(clientDetails.getAddress());
-            updatedClient.setBirthdate(clientDetails.getBirthdate());
-            return ResponseEntity.ok(clientRepository.save(updatedClient));
-        } else {
-            return ResponseEntity.notFound().build();
+    @PutMapping
+    public ResponseEntity<Client> updateClient(@RequestBody Client newClient) {
+        try {
+            if (clientService.getClient(newClient.getId()) == null) {
+                return ResponseEntity.notFound().build();
+            }
+            return ResponseEntity.ok(clientService.updateClient(newClient));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).headers(errorHeader(e)).build();
+        }
+    }
+
+    @PatchMapping
+    public ResponseEntity<Client> dynamicUpdateClient(@RequestBody Client newClient) {
+        try {
+            Client oldClient = clientService.getClient(newClient.getId());
+            if (oldClient == null) {
+                return ResponseEntity.notFound().build();
+            }
+            return ResponseEntity.ok(clientService.dynamicUpdateClient(oldClient, newClient));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).headers(errorHeader(e)).build();
         }
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteClient(@PathVariable Integer id) {
-        Optional<Client> client = clientRepository.findById(id);
-        if (client.isPresent()) {
-            clientRepository.delete(client.get());
-            return ResponseEntity.noContent().build();
-        } else {
-            return ResponseEntity.notFound().build();
+        try {
+            if (id != null) {
+                clientService.deleteClient(id);
+                return ResponseEntity.noContent().build();
+            } else {
+                return ResponseEntity.notFound().build();
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).headers(errorHeader(e)).build();
         }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Client> getClient(@PathVariable Integer id) {
+        try {
+            Client client = clientService.getClient(id);
+            if (client != null) {
+                return ResponseEntity.ok(client);
+            } else {
+                return ResponseEntity.notFound().build();
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).headers(errorHeader(e)).build();
+        }
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity<List<Client>> getAllClients() {
+        try {
+            List<Client> clientList = clientService.getAllClients();
+            if (clientList.isEmpty()) {
+                return ResponseEntity.notFound().build();
+            } else {
+                return ResponseEntity.ok(clientList);
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).headers(errorHeader(e)).build();
+        }
+    }
+
+    /**
+     * Add error message to header and log Exception thrown.
+     * @param e Exception thrown
+     * @return HttpHeader
+     */
+    private HttpHeaders errorHeader(Exception e) {
+        System.getLogger(e.toString());
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("X-Error-Message", e.getMessage());
+        return headers;
     }
 }
