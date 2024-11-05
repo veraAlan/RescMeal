f3823eb2b0dc0fc30d6efdbea2d47021d9fd4cb5
Alan Cristian Gaston Vera <vera.alan2207@gmail.com>
2024-11-05 05:06:19 -0300

    SOAP hook file for last commit.

---
+++ Frontend/src/hooks/SOAP/requestUser.tsx
@@ -0,0 +1,29 @@
+import axios from 'axios'
+const SOAP_URL = 'http://localhost:8080/ws'
+
+const createSoapRequest = (identifier: string, type: string, identifierType: string): string => {
+   return `<?xml version="1.0" encoding="UTF-8"?>
+   <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ws="http://rescmeal.food/ws-rescmeal"> 
+   <soapenv:Header/>
+      <soapenv:Body>
+         <ws:${type}>
+            <${identifierType}>${identifier}</${identifierType}> 
+         </ws:${type}>
+      </soapenv:Body>
+   </soapenv:Envelope>`
+}
+
+export const sendSoapRequest = async (identifier: string, type: string): Promise<string> => {
+   let identifierType = "username"
+   if (RegExp("^[\\w-.]+@([\\w-]+\\.)+[\\w-]{2,4}$").test(identifier)) {
+      identifierType = "email"
+   }
+   const soapRequest = createSoapRequest(identifier, type, identifierType)
+   try {
+      const response = await axios.post(SOAP_URL, soapRequest, { headers: { 'Content-Type': 'text/xml', } })
+      return response.data.replaceAll('<', '\n<')
+   } catch (error) {
+      console.error('SOAP Request Error:', error)
+      throw error
+   }
+}
