8a6271031e6586ab4fd3c0dcaab17804a2bc50a8
Alan Cristian Gaston Vera <vera.alan2207@gmail.com>
2024-11-16 18:37:32 -0300

    Added Pageable type to repository, need to complete the chain to show it correctly in the frontend.

--- Backend/src/main/java/dev/group21/rescmeal/repository/BusinessRepository.java
+++ Backend/src/main/java/dev/group21/rescmeal/repository/BusinessRepository.java
@@ -1,10 +1,13 @@
 package dev.group21.rescmeal.repository;
 
 import dev.group21.rescmeal.model.Business;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface BusinessRepository extends JpaRepository<Business, Long> {
+    Page<Business> findAllPageable();
+
     Optional<Business> findById(Long id);
 }
---
+++ Frontend/src/app/businesses/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import React from 'react'
+import ListBusinesses from './list'
+
+const BusinessesTest: React.FC = () => {
+   return (
+      <div>
+         <ListBusinesses />
+      </div>
+   )
+}
+
+export default BusinessesTest
