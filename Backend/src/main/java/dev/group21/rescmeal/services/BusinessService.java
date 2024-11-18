90b5427dc461b10300514ad86abf99aced72cb87
Alan Cristian Gaston Vera <vera.alan2207@gmail.com>
2024-11-18 18:13:34 -0300

    Corrections.

--- Backend/src/main/java/dev/group21/rescmeal/services/BusinessService.java
+++ Backend/src/main/java/dev/group21/rescmeal/services/BusinessService.java
@@ -3,7 +3,6 @@ package dev.group21.rescmeal.services;
 import dev.group21.rescmeal.model.Business;
 import dev.group21.rescmeal.repository.BusinessRepository;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
