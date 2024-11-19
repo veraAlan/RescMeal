78240f108c169c835db40b987662cf5e3ebc2cb7
Alan Cristian Gaston Vera <vera.alan2207@gmail.com>
2024-11-19 05:10:18 -0300

    Para los 2 commits anteriores: Creado "updateClient" y "updateCarrier" EN UserService. Pide el ID del usuario actual (Se puede mandar del front, despues podemos pasar a usar authentication) y el objeto del CLIENT CREADO o CARRIER CREADO respectivamente. Crea el link entre Usuario y (Client o Carrier).

--- Backend/src/main/java/dev/group21/rescmeal/services/UserService.java
+++ Backend/src/main/java/dev/group21/rescmeal/services/UserService.java
@@ -1,6 +1,8 @@
 package dev.group21.rescmeal.services;
 
 import dev.group21.rescmeal.model.Business;
+import dev.group21.rescmeal.model.Carrier;
+import dev.group21.rescmeal.model.Client;
 import dev.group21.rescmeal.model.User;
 import dev.group21.rescmeal.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +24,21 @@ public class UserService implements UserDetailsService {
                 .orElseThrow(() -> new UsernameNotFoundException("User not found, username: " + username));
 
         return UserDetailsImpl.build(user);
+    }
+
+    // Se puede refactorizar para hacer una sola funcion.
+    @Transactional
+    public User updateCarrier(Long id, Carrier carrier) {
+        User user = userRepository.findById(id).orElseThrow(() -> new UsernameNotFoundException("User not found, id: " + id));
+        user.setCarrier(carrier);
+        return userRepository.save(user);
+    }
+
+    @Transactional
+    public User updateClient(Long id, Client client) {
+        User user = userRepository.findById(id).orElseThrow(() -> new UsernameNotFoundException("User not found, id: " + id));
+        user.setClient(client);
+        return userRepository.save(user);
     }
 
     @Transactional
